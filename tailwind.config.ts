938d3dff970c36c49f571ac63b1fd9b0af8cd8cd
Forkyouabhi <forkyouabhi@proton.me>
2025-09-22 20:17:45 -0400

    v2

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+// tailwind.config.ts
+
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}', // This line is crucial
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',      // This line is also crucial
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+export default config
